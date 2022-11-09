6a9f074d3f7678e0bea51f132677ebb932412d81
Thania Beatriz <thaniabeatriz2017@gmail.com>
2022-11-09 18:24:41 -0300

    adicionado o numero de tentativas no jogo

--- Alura - Python 1/jogos/adivinhacao.py
+++ Alura - Python 1/jogos/adivinhacao.py
@@ -3,14 +3,28 @@ print("Bem vindo ao jogo de adivinhação")
 print("*************************************")
 
 numero_secreto = 42
+tentativas = 3
+rodada = 1
 
-chute_str = input("Digite o seu numero: ")
-print("Voce digitou ", chute_str)
-chute = int(chute_str)
-
-if(numero_secreto == chute):
-    print("Voce acertou!")
-else:
-    print("Voce errou!")
+while(rodada <= tentativas):
     
+    print("\nRodada {} de {}".format(rodada, tentativas))
+    chute_str = input("Digite o seu numero: ")
+    print("Voce digitou ", chute_str)
+    chute = int(chute_str)
+
+    acertou = chute == numero_secreto
+    maior   = chute > numero_secreto
+    menor   = chute < numero_secreto
+
+    if(acertou):
+        print("Voce acertou!")
+    else:
+        if(maior):
+            print("Voce errou! O seu chute foi maior que o numero secreto")
+        elif(menor):
+            print("Voce errou! O seu chute foi menor que o numero secreto")
+    
+    rodada = rodada + 1
+
 print("Fim do jogo!")
