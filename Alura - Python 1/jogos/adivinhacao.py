45c782190673789cb14172bb835cdd9b0102f0ec
Thania Beatriz <thaniabeatriz2017@gmail.com>
2022-11-15 13:08:59 -0300

    Colocando o numero secreto pra ser aleatorio

--- Alura - Python 1/jogos/adivinhacao.py
+++ Alura - Python 1/jogos/adivinhacao.py
@@ -1,17 +1,21 @@
+import random
+
 print("*************************************")
 print("Bem vindo ao jogo de adivinhação")
 print("*************************************")
 
-numero_secreto = 42
+numero_secreto = random.randrange(1,101)
 tentativas = 3
-rodada = 1
 
-while(rodada <= tentativas):
-    
+for rodada in range(1,tentativas+1)  : 
     print("\nRodada {} de {}".format(rodada, tentativas))
-    chute_str = input("Digite o seu numero: ")
+    chute_str = input("Digite um número entre 1 e 100: ")
     print("Voce digitou ", chute_str)
     chute = int(chute_str)
+
+    if(chute < 1 or chute > 101):
+        print("Você deve digitar um número entre 1 e 100!")    
+        continue
 
     acertou = chute == numero_secreto
     maior   = chute > numero_secreto
@@ -19,12 +23,11 @@ while(rodada <= tentativas):
 
     if(acertou):
         print("Voce acertou!")
+        break
     else:
         if(maior):
             print("Voce errou! O seu chute foi maior que o numero secreto")
         elif(menor):
             print("Voce errou! O seu chute foi menor que o numero secreto")
-    
-    rodada = rodada + 1
 
 print("Fim do jogo!")
